c2903b59d10fb6731888d5bcf1348fc75692a10b
Brian Nelson <brian.allen.nilsson@gmail.com>
2019-05-15 16:51:37 -0400

    Creates object with pairs and date in localstorage

--- app/app.js
+++ app/app.js
@@ -13,51 +13,57 @@
 //get item
 var getItem = function(key) {
   return window.localStorage.getItem(key);
-}
+};
 
 //create
 var createItem = function(key, value) {
-  return window.localStorage.setItem(key, value);
-}
+  return window.localStorage.setItem(key, JSON.stringify(value));
+};
 
 //update
 var updateItem = function(key, value) {
   return window.localStorage.setItem(key, value);
-}
+};
 
 //delete
 var deleteItem = function(key) {
   return window.localStorage.removeItem(key);
-}
+};
 
 //clear everything
 var clearEverything = function() {
   return window.localStorage.clear();
-}
+};
 
 var keyExists = function(key) {
   var currentValue = getItem(key);
   return currentValue !== null;
-}
-
+};
 
 ///////////////////////////////////////////
 //event handlers for the buttons and ... possibly the inputboxes
-  //preventdefault on button clicks
+//preventdefault on button clicks
 $(document).ready(function() {
-  $('#createButton').click(function(event) {
+  $("#startButton").click(function(event) {
     event.preventDefault();
 
-    var currentKey = $("#keyInput").val();
-    var currentValue = $("#valueInput").val();
-    if (keyExists(currentKey)) {
-      //current key exists, do something error-handle-y
-    } else {
-      createItem(currentKey, currentValue);
-    }
+    const driver = $("#driver").val();
+    const navigator = $("#navigator").val();
+    const length = $("#length").val();
+    const numPeriods = $("#periods").val();
+
+    let obj = {
+      driver: driver,
+      navigator: navigator,
+      startTime: Date(),
+      length: length,
+      periods: numPeriods
+    };
+    createItem("pomodoro", obj);
+    //}
   });
 
-  $('#updateButton').click(function(event) {
+  $("#updateButton").click(function(event) {
     event.preventDefault();
 
     var currentKey = $("#keyInput").val();
