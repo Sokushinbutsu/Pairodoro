47a419dee6281c3b91ca07bf0ad625d98b32a9f2
Brian Nelson <brian.allen.nilsson@gmail.com>
2019-08-14 19:32:08 -0400

    deploying static assets with express

--- server.js
+++ server.js
@@ -7,9 +7,14 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
-app.use(express.static("./frontend/src"));
 app.use(bp.urlencoded({ extended: false }));
 app.use(bp.json());
+
+app.use(express.static(path.join(__dirname, "frontend", "build")));
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
 
 app.use("/api/pomodoros", pomodoros);
 
