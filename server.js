600348ad5ef03ff35c265f469bd5416d89d83ecc
Brian Nelson <brian.allen.nilsson@gmail.com>
2019-08-14 19:23:42 -0400

    moved mongoURI to env

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const express = require('express');
-const bp = require('body-parser');
-const pomodoros = require('./routes/api/pomodoros');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const bp = require("body-parser");
+const pomodoros = require("./routes/api/pomodoros");
+const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
-app.use(express.static('./frontend/src'));
+app.use(express.static("./frontend/src"));
 app.use(bp.urlencoded({ extended: false }));
 app.use(bp.json());
 
-app.use('/api/pomodoros', pomodoros);
+app.use("/api/pomodoros", pomodoros);
 
 const port = process.env.PORT || 4000;
 
