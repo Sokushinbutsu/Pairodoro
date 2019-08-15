455fd4f7a0b9529808adbb2c3b0838ddc9a3c5c7
Brian Nelson <brian.allen.nilsson@gmail.com>
2019-08-15 13:45:33 -0400

    fixed parameters on post req to github

--- server.js
+++ server.js
@@ -14,19 +14,26 @@ app.use(bp.json());
 
 app.use(express.static(path.join(__dirname, "frontend", "build")));
 
-app.get("*", function(req, res) {
+app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
 });
 
-app.post("/authenticate", (req, res) => {
+app.get("/authenticate/:code", (req, res) => {
   // POST https://github.com/login/oauth/access_token
   // This route takes in a code generated from a successful github login
   // and exchanges that code for a token we can use to access the API
-  Axios.post(`https://github.com/login/oauth/${req.body.code}`).then(
-    response => {
-      console.log(response);
-    }
-  );
+  Axios.post(`https://github.com/login/oauth/access_token`, {
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    code: req.params.code
+  })
+    .then(response => {
+      res.status(200).send(response);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+      console.error(error);
+    });
 });
 
 app.use("/api/pomodoros", pomodoros);
